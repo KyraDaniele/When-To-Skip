868c4fed3e704cea1ef2e2c5f7f740db26469bbf
Kyra Mellema <kemellema@gmail.com>
2020-02-19 18:45:26 -0500

    Outlining schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,22 @@
 Database triggerTest
 
+-- TODO:
+-- Users can rate media, view what they've rated, edit rating, comment on media, delete comment
+
+
 CREATE TABLE user (
     id SERIAL PRIMARY KEY,
     username VARCHAR (50) NOT NULL,
-    
+    rated
+    commented
+);
+
+CREATE TABLE media (
+    id SERIAL PRIMARY KEY,
+    title varchar (200) NOT NULL,
+    info varchar NOT NULL,
+    category varchar NOT NULL,
+    rating 
+    comment
+    user_id foreign key user(id)
 );
